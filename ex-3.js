41400055c8d4f511da78a27df0d8f5005910a6f7
lalinyuuu <lullalin.yu@gmail.com>
2025-07-30 14:27:03 +0700

    ex1-7

--- ex-3.js
+++ ex-3.js
@@ -9,6 +9,9 @@ let additionalOrders = [
   { orderId: "A005", paymentType: "Credit Card", totalPrice: 45000 },
 ];
 
+
 // Start coding here
+let newOrders = [...orders, ...additionalOrders];
+
 
 console.log(newOrders);
